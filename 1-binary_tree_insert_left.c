39980427204b4237e7cda5c57a57762710df5f2e
17Keith <kayomega7@gmail.com>
2022-08-23 11:11:55 +0300

    BT [Fix Errors]

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,19 +12,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if(!parent)
+	if (!parent)
 		return (NULL);
 
 	new = malloc(sizeof(binary_tree_t));
-	if(!new)
-		return(NULL);
+	if (!new)
+		return (NULL);
 
 	new->left = parent->left;
 	new->n = value;
 	new->parent = parent;
 	new->right = NULL;
-	if(new->left)
+	if (new->left)
 		new->left->parent = new;
-	
-	return(new);
+
+	return (new);
 }
