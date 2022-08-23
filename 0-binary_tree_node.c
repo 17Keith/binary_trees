c0c6f41328c0272f2aef3cea61d2e68ae5640d8b
17Keith <kayomega7@gmail.com>
2022-08-23 10:42:10 +0300

    BT [Fix Errors]

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,22 +2,24 @@
 #include <stddef.h>
 
 /**
- * @parent: is a pointer to the parent node
- * @value: is the value to put in the new node
+ * binary_tree_node - creates a node.
+ * @parent : is a pointer to the parent node
+ * @value : is the value to put in the new node
  * Return: pointer to the new node or NULL on failure.
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
-    new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
-    if (!new)
-        return (NULL);
+	new = malloc(sizeof(binary_tree_t));
 
-    new->n = value;
-    new->left = NULL;
-    new->right = NULL;
-    new->parent = parent;
-    return (new);
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	return (new);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -16,10 +16,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -28,7 +28,8 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-//Prototypes for the first 18 tasks!
+/*Prototypes for the first 18 tasks!*/
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
