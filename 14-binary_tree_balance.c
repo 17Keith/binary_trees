915722229d5e2a4ff0ad3500e4c9281e42058424
17Keith <kayomega7@gmail.com>
2022-08-25 01:15:57 +0300

    BT

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,10 +9,10 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int height_l, height_r;
+
 	if (!tree)
 	return (0);
-
-	int height_l, height_r;
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
