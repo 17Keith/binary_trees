50829ccc25616a68bc8cd3ec3f698a319c521931
17Keith <kayomega7@gmail.com>
2022-08-24 15:15:22 +0300

    BT [Fix Errors]

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,13 +9,23 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	/* size_t depth_l = 0;
+	size_t depth_r = 0;
+	 */
+	size_t depth = 0;
+
 	if (!tree)
 	return (0);
 
-	size_t depth_l = 0;
-	size_t depth_r = 0;
-
-	depth_l = tree->left ?  1 + binary_tree_depth(tree->left) : 0;
-	depth_r = tree->right ?  1 + binary_tree_depth(tree->right) : 0;
+	/* depth_l = tree->left ? 1 + binary_tree_depth(tree->left) : 0;
+	depth_r = tree->right ? 1 + binary_tree_depth(tree->right) : 0;
 	return (depth_l > depth_r ? depth_l : depth_r);
+ */
+	while (tree->parent)
+		{
+			depth++;
+			tree = tree->parent;
+		}
+
+		return (depth);
 }
